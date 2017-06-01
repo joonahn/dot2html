53be8069d897f5d38c12e11f8995b43d4c657f06
joonahn <sdeveloper@naver.com>
2017-06-01 15:33:32 +0900

    finished

--- dot2html.py
+++ dot2html.py
@@ -13,7 +13,8 @@ def parsedot(filename):
 		f.readline()
 		while True:
 			line = f.readline()
-			if not line: break
+			if not line: 
+				break
 			if re.match('.+(?=\[)', line) or re.match('(?<=\[).*(?=\])', line):
 				id_info = re.findall('.+(?=\[)', line)[0].strip()
 				meta_info = re.findall('(?<=\[).*(?=\])', line)[0].strip()
@@ -26,9 +27,9 @@ def parsedot(filename):
 					outdict["links"].append({"from":from_info, "to":to_info})
 	return outdict
 
-def getVertexWithID(id, vertices):
+def getVertexWithID(_id, vertices):
 	for vertex in vertices:
-		if vertex["id"] == id:
+		if vertex["id"] == _id:
 			return vertex
 	raise  Exception("No such vertex exists!")
 
@@ -51,12 +52,20 @@ def computeSCCs(graph):
 	pop = stack.pop
 
 	for vertex in vertices:
-		if not "index" in vertex:
-			scc, index = computeSCC(vertex, graph, push, pop, index)
-			if len(scc) != 0:
-				sccs.append(scc)
+		if not ("index" in vertex):
+			newsccs, index = computeSCC(vertex, graph, push, pop, index)
+			sccs  = sccs + newsccs
+
+	# Remove singletons
+	realsccs = []
+	for scc in sccs:
+		if len(scc) > 1:
+			realsccs.append(scc)
+
+	return realsccs
 
 def computeSCC(vertex, graph, push, pop, index):
+	sccs = []
 	vertices = graph["nodes"] # ["id"]
 	edges = graph["links"] # ["from"] ["to"]	
 	vertex["index"] = index
@@ -67,24 +76,23 @@ def computeSCC(vertex, graph, push, pop, index):
 
 	for w in findNeibors(vertex, graph):
 		if not "index" in w:
-			computeSCC(w, graph, push, pop, index)
+			newsccs, index = computeSCC(w, graph, push, pop, index)
+			sccs = sccs + newsccs
 			vertex["lowlink"] = min([vertex["lowlink"], w["lowlink"]])
 		elif w["onstack"]:
 			vertex["lowlink"] = min([vertex["lowlink"], w["index"]])
 
-	scc = []
 	if vertex["lowlink"] == vertex["index"]:
+		scc = []
 		while True:
 			w = pop()
 			w["onstack"] = False
 			scc.append(w)
 			if w["id"] == vertex["id"]:
 				break
-
-	return scc, index
-
-
-
+		if len(scc) != 0:
+			sccs.append(scc)
+	return sccs, index
 
 def nodesToStr(nodelist):
 	map_fcn = lambda x: '\t{"id":"' + x["id"] + '","label":"' + x["label"] + '"}'
@@ -96,11 +104,25 @@ def linksToStr(linklist):
 	nodeStrList = list(map(map_fcn, linklist))
 	return "[\n" + (",\n".join(nodeStrList)) + "]"
 
+def SCCsToStr(sccs):
+	map_fcn = lambda (i,x): '"scc' + str(i) + '":["' + '","'.join([item["id"] for item in x]) + '"]'
+	sccStrList = list(map(map_fcn, enumerate(sccs)))
+	return "{" + (",\n".join(sccStrList)) + "}"
+
 infilename = sys.argv[1]
 outfilename = os.path.splitext(infilename)[0] + '.html'
 templatename = "dagre-d3.html"
 
 parsedData = parsedot(infilename)
+
+sccs = computeSCCs(parsedData)
+
+for scc in sccs:
+	print "newscc:",
+	for node in scc:
+		print node["id"] + ", ",
+	print ""
+
 
 with open (outfilename, 'w') as f:
 	with open (templatename, 'r') as tp:
@@ -110,10 +132,13 @@ with open (outfilename, 'w') as f:
 			if "####" in line:
 				nodeStr = nodesToStr(parsedData["nodes"])
 				linkStr = linksToStr(parsedData["links"])
+				sccStr = SCCsToStr(sccs)
 				f.write("var nodes = ")
 				f.write(nodeStr + "\n")
 				f.write(",\n links = ")
-				f.write(linkStr + ";\n")
+				f.write(linkStr + "\n")
+				f.write(",\n SCClist = ")
+				f.write(sccStr + ";\n")
 			else:
 				f.write(line)
 
